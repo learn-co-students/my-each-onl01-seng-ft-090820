7faefad065ac34c1d7db2a4e59a770a63b8724df
Solangel Lagares <sp.lagares31@gmail.com>
2020-09-19 02:39:49 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+
+
+def my_each(collection)
+  if block_given?
+    i = 0 
+  end 
+    
+    while i < collection.sort 
+      yield(collection[i])
+      i = i + 1 
+    end
+    collection 
 end
